55f1d892b035e40be505fd57b7f1b8ee4902ef33
Nirosh Suthagar <nirosh.suthagar@qa.com>
2025-12-04 15:13:46 +0000

    Updated Notes, End of Day 2

---
+++ Student Pack/Python/.py demos/best_practise_app.py
@@ -0,0 +1,7 @@
+def sayHello():
+    print("Say Hello to the user...")
+
+if __name__ == "__main__":
+    sayHello()
+
+    main()
---
+++ Student Pack/Python/.py demos/myfirstapp.py
@@ -0,0 +1,13 @@
+def sayHello():
+    print("Hello world V2")
+
+sayHello()
+
+
+
+
+
+
+
+
+
